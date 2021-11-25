d19cc05ec4f3e6b612bf98d0350e82a569e62301
Kyungwoon Na <nakw0410@gmail.com>
2021-11-25 15:29:29 +0900

    feat: 1~9까지 랜덤 숫자 생성 메소드 구현

--- src/숫자야구게임/Main.java
+++ src/숫자야구게임/Main.java
@@ -1,9 +1,29 @@
 package 숫자야구게임;
 
 public class Main {
-
+	/*
+	 *메소드 리스트
+	 1. 1부터 9까지 중복없는 3개의 랜덤 숫자 생성 기능 : makeRandNum
+	 */
+	public static int[] makeRandNum() {
+		int [] randNum = new int [3];
+		
+		for(int i = 0; i < randNum.length; i++) {
+			randNum [i] = (int)(Math.random()*9+1);
+			for(int j = 0; j < i; j++) {
+				if(randNum[i] == randNum[j]) {
+					i--;
+				}
+			}
+		}
+		return randNum;
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//makeRandNum(중복 숫자) 테스트
+		int [] arr = makeRandNum();
+		for(int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
+		}
 
 	}
 
